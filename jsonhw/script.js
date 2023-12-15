9dc1e980776648b7735364fa4a9f956b046b8119
Enes Şahin <enessahin9@outlook.com>
2023-12-15 16:20:30 +0300

    js5

--- jsonhw/script.js
+++ jsonhw/script.js
@@ -1,3 +1,72 @@
+// function fetchData(resource) {
+//     const url = `https://jsonplaceholder.typicode.com/${resource}`;
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => displayTable(data, resource))
+//         .catch(error => console.error('Error fetching data:', error));
+// }
+
+// function displayTable(data, resource) {
+//     const tableContainer = document.getElementById('table-container');
+//     tableContainer.innerHTML = ''; // Clear previous content
+
+//     const table = document.createElement('table');
+//     const headerRow = document.createElement('tr');
+
+//     // Create table headers
+//     Object.keys(data[0]).forEach(key => {
+//         const th = document.createElement('th');
+//         th.textContent = key;
+//         headerRow.appendChild(th);
+//     });
+
+//     table.appendChild(headerRow);
+
+//     // Create table rows
+//     data.forEach(item => {
+//         const row = document.createElement('tr');
+
+//         Object.entries(item).forEach(([key, value]) => {
+//             const td = document.createElement('td');
+
+//             if (resource === 'users' && (key === 'address' || key === 'company')) {
+//                 // Handle special cases for 'address' and 'company' fields
+//                 if (key === 'address') {
+//                     const addressString = `${value.city}, ${value.street}, ${value.suite}, ${value.zipcode}`;
+//                     td.textContent = addressString;
+//                 } else if (key === 'company') {
+//                     td.textContent = value.name;
+//                 }
+//             } else if (resource === 'photos' && key === 'thumbnailUrl') {
+//                 const thumbnail = document.createElement('img');
+//                 thumbnail.src = value;
+//                 thumbnail.alt = 'Thumbnail';
+//                 thumbnail.addEventListener('click', () => openPhotoInNewTab(item.url));
+//                 td.appendChild(thumbnail);
+//                 // Add click event to the entire now
+//                 row.addEventListener('click', () => openPhotoInNewTab(item.url));
+//             } else {
+//                 td.textContent = value;
+//             }
+
+//             row.appendChild(td);
+//         });
+
+//         table.appendChild(row);
+//     });
+
+//     tableContainer.appendChild(table);
+// }
+
+// function openPhotoInNewTab(url) {
+//     window.open(url, '_blank');
+// }
+
+// function handleCellClick(content) {
+//     // Example: Open a new tab with the content when a cell is clicked
+//     window.open(content, '_blank');
+// }
+
 function fetchData(resource) {
     const url = `https://jsonplaceholder.typicode.com/${resource}`;
     fetch(url)
@@ -43,7 +112,7 @@ function displayTable(data, resource) {
                 thumbnail.alt = 'Thumbnail';
                 thumbnail.addEventListener('click', () => openPhotoInNewTab(item.url));
                 td.appendChild(thumbnail);
-                // Add click event to the entire now
+                // Add click event to the entire row
                 row.addEventListener('click', () => openPhotoInNewTab(item.url));
             } else {
                 td.textContent = value;
@@ -51,6 +120,11 @@ function displayTable(data, resource) {
 
             row.appendChild(td);
         });
+
+        // Add a click event to the entire row for Albums
+        if (resource === 'albums') {
+            row.addEventListener('click', () => openAlbumPhotos(item.userId));
+        }
 
         table.appendChild(row);
     });
@@ -62,7 +136,45 @@ function openPhotoInNewTab(url) {
     window.open(url, '_blank');
 }
 
-function handleCellClick(content) {
-    // Example: Open a new tab with the content when a cell is clicked
-    window.open(content, '_blank');
+function openAlbumPhotos(userId) {
+    const photosUrl = `https://jsonplaceholder.typicode.com/photos?albumId=${userId}`;
+    fetch(photosUrl)
+        .then(response => response.json())
+        .then(data => displayTable(data.map(e => ({albumId: e.albumId, thumbnailUrl: e.thumbnailUrl})), 'photos'))
+        .catch(error => console.error('Error fetching album photos:', error));
+}
+function openAlbumPhotos(albumId) {
+    const photosUrl = `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
+    fetch(photosUrl)
+        .then(response => response.json())
+        .then(data => displayPhotoPopup(data))
+        .catch(error => console.error('Error fetching album photos:', error));
+}
+function displayPhotoPopup(photos) {
+    const popupContainer = document.createElement('div');
+    popupContainer.classList.add('popup-container');
+
+    photos.forEach(photo => {
+        const photoPopup = document.createElement('div');
+        photoPopup.classList.add('photo-popup');
+
+        const title = document.createElement('h2');
+        title.textContent = photo.title;
+
+        const img = document.createElement('img');
+        img.src = photo.url;
+        img.alt = photo.title;
+
+        photoPopup.appendChild(title);
+        photoPopup.appendChild(img);
+
+        popupContainer.appendChild(photoPopup);
+    });
+
+    document.body.appendChild(popupContainer);
+
+    // Popup'ı kapatmak için bir event listener ekle
+    popupContainer.addEventListener('click', () => {
+        document.body.removeChild(popupContainer);
+    });
 }
